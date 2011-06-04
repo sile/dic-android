9f32d26a45ff2d2092a2f13005541bd9a18412ab
sile <phjgt308@gmail.com>
2011-06-04 12:31:42 +0900

    いくつかファイル追加

---
+++ diclookup/src/net/reduls/diclookup/Char.java
@@ -0,0 +1,12 @@
+package net.reduls.diclookup;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+
+public final class Char {
+    private final char[] charCode;
+    
+    public Char(String dictionaryDirectory) throws IOException {
+        
+    }
+}
