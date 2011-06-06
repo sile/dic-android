6969a4908a7ef298bd3419f4f90afaebd3fe8e92
sile <phjgt308@gmail.com>
2011-06-07 01:15:58 +0900

    結果　画面タイトルを動的に変更(ワード名に)

--- app/src/net/reduls/dic/Entry.java
+++ app/src/net/reduls/dic/Entry.java
@@ -4,6 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.text.style.ClickableSpan;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.content.Intent;
 
 public class Entry extends Activity {
     public net.reduls.diclookup.Dic dic;
@@ -17,8 +24,33 @@ public class Entry extends Activity {
         net.reduls.diclookup.Dic.Entry e = dic.getEntryFromId(getIntent().getIntExtra("entry.id",0));
 
         TextView entryView = (TextView)findViewById(R.id.entry_body);
-        entryView.setText(e.data);
 
-        // TODO: タイトル変更
+        setTitle(e.title);
+
+        //
+        Spannable spannable = new SpannableString(e.data);
+        spannable.setSpan(new CS(this,0,10), 0, 10, 
+                          Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        entryView.setText(spannable, TextView.BufferType.SPANNABLE);
+        entryView.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    private class CS extends ClickableSpan {
+        private Activity ac;
+        private int start,end;
+        public CS(Activity ac, int s, int e) {
+            super();
+            this.ac = ac;
+            start = s;
+            end = e;
+        }
+        
+        public void onClick(View v) {
+            Intent i = new Intent(ac, Dic.class);
+            i.putExtra("search.key", ((TextView)v).getText().toString().substring(start,end));
+            startActivity(i);            
+        }
+        
+        //public void updateDrawState(TextPaint ds) {}
     }
 }
